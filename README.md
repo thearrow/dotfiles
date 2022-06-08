bc397d22f6c94454b16927aa3fd46ab12161aca0
Jake Klingler <thearrow3456@gmail.com>
2022-06-08 19:54:53 -0400

    more files

---
+++ README.md
@@ -0,0 +1,15 @@
+# Setup
+
+```shell
+# install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+# install chezmoi
+brew install chezmoi
+
+# install dotfiles
+chezmoi init --apply --verbose https://github.com/thearrow/dotfiles.git
+
+# run setup script
+./setup.sh
+```
--- executable_setup.sh
+++ executable_setup.sh
@@ -1,20 +1,5 @@
 #!/usr/bin/env bash
 
-if ! command -v brew &> /dev/null
-then
-    # install homebrew
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-fi
-
-if ! command -v chezmoi &> /dev/null
-then
-    # install chezmoi
-    brew install chezmoi
-fi
-
-# install dotfiles
-chezmoi init --apply --verbose https://github.com/thearrow/dotfiles.git
-
 # install software deps from Brewfile
 brew bundle
 
