b8bfeaabe3a92865ab004a014a5bcf2e49752703
Jake Klingler <thearrow3456@gmail.com>
2023-02-24 13:09:12 -0500

    znap migration

--- executable_setup.sh
+++ executable_setup.sh
@@ -6,12 +6,6 @@ brew bundle
 # fzf post-install
 bash -c $(brew --prefix)/opt/fzf/install
 
-# install zinit
-bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
-
-# enable brew completions
-zinit creinstall $(brew --prefix)/share/zsh/site-functions
-
 # quiet new shell
 touch ~/.hushlogin
 
